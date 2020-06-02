6c218a1a9ed79a0348a9e53c191984884c93d4b6
wan9wu <bcsxmail@gmail.com>
2020-06-02 22:18:18 +0800

    更新内容

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         var DataContentId = "#" + $(this).attr("data-content-id");
         var t = $(DataContentId).offset().top;//  获取需要跳转到标签的top值
         console.log(DataContentId,t,$('#details').scrollTop());
-        $("#details").animate({ scrollTop: $('#details').scrollTop()+t-60}, 1000); // 动态跳转到指定位置（数值越大滚动速度越慢）
+        $("#details").animate({ scrollTop: $('#details').scrollTop()+t-30}, 1000); // 动态跳转到指定位置（数值越大滚动速度越慢）
         
         
     });
